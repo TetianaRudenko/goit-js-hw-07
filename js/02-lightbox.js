c9b7862a89791fc63f20c974d1c1d8ae2e95ece6
TetianaRudenko <fyilibratgif@gmail.com>
2023-03-01 16:31:32 +0200

    done HW task02
    1.create and rend markup
    2.install SimpleLightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryContainer = document.querySelector('.gallery');
+const gallaryMarkup = createGallaryMarkup(galleryItems); 
+
+galleryContainer.insertAdjacentHTML('beforeend', gallaryMarkup);
+
+function createGallaryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+    `}).join("");
+  }
+
+let lightbox = new SimpleLightbox('.gallery a', { caption: true, captionSelector: 'img[alt]', captionType: 'attr', captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250 }); 
+
 console.log(galleryItems);
+
